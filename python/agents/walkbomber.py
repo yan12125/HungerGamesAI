f72193e03e6a53b2006f542ec45b020dd8761400
b00901157 <b00901157@ntu.edu.tw>
2014-12-28 10:41:39 +0800

    delete that fucking state machine

--- python/agents/walkbomber.py
+++ python/agents/walkbomber.py
@@ -16,6 +16,9 @@ class WalkbomberAgent(Agent):
         move = self.lastMove
 
         safe_map = state.game_map.safeMap()
+        def __internal_safe(pos):
+            gridX, gridY = util.posToGrid(pos)
+            return safe_map[gridX][gridY]
 
         player = state.me()
 
@@ -24,12 +27,10 @@ class WalkbomberAgent(Agent):
 
         playerPos = util.coordToPos(player.x, player.y)
         gridX, gridY = util.posToGrid(playerPos)
-        bombPos = util.gridToPos(gridX, gridY)
-
-        if self.whichState == 0 and self.lastState == 1 and bombPos != self.lastPos:
-            self.whichState = 2
-        elif self.whichState == 0 and self.lastState == 2 and safe_map[gridX][gridY] == True:
-            self.whichState = 3
+ 
+        actions = search.bfs(state.game_map, playerPos, __internal_safe)
+        if actions:
+            move = actions[0]
 
         validMoves = state.validMovesForMe()
         if Direction.STOP in validMoves:
@@ -38,39 +39,8 @@ class WalkbomberAgent(Agent):
         if not validMoves:
             print('Error: no valid moves')
             return
-
-        # can we stop?
-
-        print(self.whichState)
-        # run away from my own bomb
-        if self.whichState == 1:
-            if move == Direction.STOP:
-                move = random.choice(validMoves)
-            self.whichState = 0
-            self.lastState = 1
-        # run away from unsafe place
-        elif self.whichState == 2:
-            moves = [m for m in validMoves if m != move and m != oppDirection(move)]
-            if moves:
-                move = random.choice(moves)
-            self.whichState = 0
-            self.lastState = 2
-        elif self.whichState == 3:
-            if not validMoves:
-                print('Error: no valid moves')
-                return
-            else:
-                def __internal_safe(pos):
-                    gridX, gridY = util.posToGrid(pos)
-                    return safe_map[gridX][gridY]
-                actions = search.bfs(state.game_map, playerPos, __internal_safe)
-                if actions:
-                    move = actions[0]
-
+        
         if not state.moveValidForMe(move):
-            if not validMoves:
-                print('Error: no valid moves')
-                return
             move = random.choice(validMoves)
 
         self.lastMove = move
