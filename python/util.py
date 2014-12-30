ecefc3922d928bb4f1ee4268de9b97a559b06b94
dodophys <chto@asiaa.sinica.edu.tw>
2014-12-30 12:42:14 +0800

    add some function

--- python/util.py
+++ python/util.py
@@ -103,3 +103,8 @@ def empty_map():
 def mark_finished():
     loop.add_finished_task()
     packet_queue.put(None)
+
+def manhattanDistance( xy1, xy2 ):
+    "Returns the Manhattan distance between points xy1 and xy2"
+    return abs( xy1[0] - xy2[0] ) + abs( xy1[1] - xy2[1] )
+
