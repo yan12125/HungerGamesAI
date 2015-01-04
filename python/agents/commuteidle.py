3585f1d49d3032e7090ff124e870ad6fc9811fe7
dodophys <chto@asiaa.sinica.edu.tw>
2015-01-04 20:09:33 +0800

    Modified some method

--- python/agents/commuteidle.py
+++ python/agents/commuteidle.py
@@ -1,23 +1,67 @@
 from .agent import Agent
 import util
+import random
+from direction import Direction
+
 
 class CommuteidleAgent(Agent):
     def __init__(self):
         super(CommuteidleAgent, self).__init__()
         self.friendId=[]
+    def sendAdviceFriend(self,myID,friendID,advice):
+      '''This method implement send message to friend
+         send advice (move,PutBomb) to friend
+      '''
+
+      util.commute_packet_queue.put({
+                'event': 'Advice',
+                'myPlayerid': myID,
+                'friendPlayerid': friendID,
+                'Move': advice
+      })
+    def registerFriend(self,myID,friendID):
+      util.commute_packet_queue.put({
+              'event': 'register_friend',
+              'myPlayerid': myID,
+              'friendPlayerid': friendID
+      })
+    def checkNone(self,player):
+      if player.friendId==[]:
+          player.friendId=self.friendId
+
+      if len(player.friendId)>1 and player.friendId[0]=='None':
+         player.friendId.remove('None')
+    def checkValidId(self,IdList,state):
+       if not IdList : return 
+       for ID in IdList:
+         try:
+            state.players[ID]
+         except:
+            print "False"
+            state.me().friendId.remove(ID)
+            return 
+       return 
 
     def once(self, state):
         player=state.me()
-        if player.friendId==[]:
-          player.friendId=self.friendId
+        myID=player.thisPlayer_id
+        self.checkNone(player)
+        move = self.lastMove
+        self.checkValidId(player.friendId,state)
         if player.friendId:
-#          print player.thisPlayer_id
-          util.commute_packet_queue.put({
-              'event': 'register_friend',
-              'myPlayerid': player.thisPlayer_id,
-              'friendPlayerid': player.friendId[0]
-          })
-          if len(player.friendId)>1 and player.friendId[0]=='None':
-            self.friendId.remove('None')
+          friendId=player.friendId[0]
+          validMoveforFriend=state.validMovesForPlayer(friendId)
+          if Direction.STOP in validMoveforFriend: 
+            validMoveforFriend.remove(Direction.STOP)
+          if validMoveforFriend:
+            move = random.choice(validMoveforFriend)
+            putBomb=False
+            self.sendAdviceFriend(myID,friendId,str((move,putBomb)))
+    def coordinate(self,state,friendId): 
+        raise NotImplementedError("Please Implement this method")
+
+
+
+
 
 
