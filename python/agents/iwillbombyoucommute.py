961023ee22100cef7dc63a3e6896e20b53d8fdcd
dodophys <chto@asiaa.sinica.edu.tw>
2015-01-06 22:59:10 +0800

    Fix some bugs:)

--- python/agents/iwillbombyoucommute.py
+++ python/agents/iwillbombyoucommute.py
@@ -17,12 +17,26 @@ class IwillbombyoucommuteAgent(CommuteidleAgent):
     def coordinate(self,state,friendId):
         putBomb=False
         myID=state.me().thisPlayer_id
-        move = self.lastAdvice
         player = state.players[friendId]
         myMap = state.game_map
         safe_map = myMap.safeMap()
         playerPos = util.coordToPos(player.x, player.y)
-        gridX, gridY = util.posToGrid(playerPos)
+        gridX, gridY = util.posToGrid(playerPos)                
+        move = self.lastMove
+        if not state.moveValidForMe(move):
+            validMoves = state.validMovesForMe()
+            if Direction.STOP in validMoves:
+                # Not always true. Eg., on a newly put bomb
+                validMoves.remove(Direction.STOP)
+            if not validMoves:
+                print('Error: no valid moves')
+                return
+            move = random.choice(validMoves)
+ 
+
+
+
+
         def __internal_safe(pos):
             gridX, gridY = util.posToGrid(pos)
             return safe_map[gridX][gridY]
@@ -46,80 +60,70 @@ class IwillbombyoucommuteAgent(CommuteidleAgent):
         def __trueCriteria(p):
             return True
 
-        moveLenth = state.tryBombConsiderOthers(__trueCriteria,friendId)
+        runActions = state.tryBombConsiderOthers(__trueCriteria,friendId)
+        moveLenth = runActions[0]
         bombTime = state.findMinBombTime()
-        if player.bombCount > 4:
-            bombTime -= 1.0
-        else:
-            bombTime -=0.5
+        bombTime -= 0.5
         judgePass = bombTime * player.speed / util.BASE_INTERVAL - moveLenth[0] * util.grid_dimension
-        if  moveLenth[0] != util.map_dimension ** 2 and\
-            (not state.bombThing(playerPos, Grid.TOOL) or __judgeStrong(player)) and\
+        if  (not state.bombThing(playerPos, Grid.TOOL) or __judgeStrong(player)) and\
             (state.bombPlayer(playerPos,friendId) or state.bombThing(playerPos, Grid.VWALL))and\
+            myMap.grids[playerPos].canPass()and \
             judgePass > 0:
             putBomb=True
+
+
         if self.goalPos != None and putBomb == False:
-            if  moveLenth[0] != util.map_dimension ** 2 and\
-            (not state.bombThing(playerPos, Grid.TOOL) or __judgeStrong(player)) and\
-            (state.bombPlayer(playerPos,friendId) or state.bombThing(playerPos, Grid.VWALL) or state.bombNearPlayer(playerPos,friendId,self.goalPos))and\
-            judgePass > 0:
+            if (not state.bombThing(playerPos, Grid.TOOL) or __judgeStrong(player)) and\
+            (state.bombPlayer(playerPos,friendId) or state.bombThing(playerPos, Grid.VWALL) or state.bombNearPlayer(playerPos,friendId,self.goalPos))and myMap.grids[playerPos].canPass() and judgePass > 0:
               putBomb=True
-        if not __judgeStrong(player):
-            actions = search.bfs(myMap, playerPos, __findTool, __findMiddleTool, player)
-            if actions:
-                move = actions[0]
-        else:
-            actions = search.bfs(myMap, playerPos, __findPlayer, __findMiddleTool, player)
-            if actions:
-                move = actions[0]
 
-        if judgePass < 0:
-            if moveLenth[0] == util.map_dimension ** 2:
-                actions = search.bfs(myMap, playerPos, __internal_safe, Player = player)
-            else:
-                actions = state.tryBombConsiderOthers(__othersCriteria)[1]
-
-            if actions:
-                move = actions[0]
-            else:
-                return
-
-#        print("Speed: %s, Limit: %s, Count: %s, Power: %s"\
-#        %(player.speed, player.bombLimit, player.bombCount, player.bombPower))
-        print("JudgePass: %s, Action: %s, Move: %s" %(judgePass, actions, move))
-        print("PLayer Pos: %s"% player.x, player.y)
-
-        if not state.moveValidForPlayer(friendId,move) and judgePass > 0:
-            centerX, centerY = util.posToCoord(playerPos)
-#            player.x = centerX
-#            player.y = centerY
-            validMoves = state.validMovesForPlayer(friendId)
-            if Direction.STOP in validMoves:
-                # Not always true. Eg., on a newly put bomb
-                validMoves.remove(Direction.STOP)
-            if not validMoves:
-                print('Error: no valid moves')
-                return
-            move = random.choice(validMoves)
         distance = Direction.distances[move]
         gridX, gridY = util.coordToGrid(player.x, player.y)
         newX = gridX + distance[0]
         newY = gridY + distance[1]
         newP = util.gridToPos(newX, newY)
-#        if (myMap.wayAroundPos(newP, player) == 0) and judgePass > 0:
-#            for d in Direction.ALL:
-#                dis = Direction.distances[d]
-#                nX = gridX + dis[0]
-#                nY = gridY + dis[1]
-#                nP = util.gridToPos(nX, nY)
-#                if myMap.wayAroundPos(nP) > 0:
-#                    move = d
-#                    break;
+        if judgePass > 0 and myMap.wayAroundPos(newP, player) > 0:
+            if not __judgeStrong(player):
+                actions = search.bfs(myMap, playerPos, __findTool, __findMiddleTool, player)
+                if actions:
+                    move = actions[0]
+            else:
+                actions = search.bfs(myMap, playerPos, __findPlayer, __findMiddleTool, player)
+                if actions:
+                    move = actions[0]
 
-        self.lastAdvice = move
-        print str((move,putBomb))
-        self.sendAdviceFriend(myID,friendId,str((move,putBomb)),str(self.goalPos))
-#        self.goMove(player, move)
+        if judgePass < 0 or myMap.wayAroundPos(newP, player) == 0:
+            actions = search.bfs(myMap, playerPos, __internal_safe, Player = player)
+            if actions:
+                move = actions[0]
+            else:
+                return
+
+        #print("Speed: %s, Limit: %s, Count: %s, Power: %s"\
+        #%(player.speed, player.bombLimit, player.bombCount, player.bombPower))
+
+        if not state.moveValidForMe(move):
+            distance = Direction.distances[move]
+            destPos = util.gridToPos(gridX + distance[0], gridY + distance[1])
+
+            def __toGrid(coord):
+                return destPos == util.coordToPos(*coord)
+
+            startCoord = (player.x, player.y)
+            actions = search.bfsPixel(state.game_map, startCoord, __toGrid, Player=player)
+            if actions:
+                move = actions[0]
+            else:
+                print("Can't reach dest grid")
+                return
+
+            if not state.moveValidForMe(move):
+                raise Exception('Unexpected: move %s should be valid' % move)
+
+        #print("JudgePass: %s, Action: %s, Move: %s" %(judgePass, actions, move))
+        self.goMove(player, move)
+        self.lastMove = move
+
     def once(self, state):
         if not util.packet_queue.empty():
             return
