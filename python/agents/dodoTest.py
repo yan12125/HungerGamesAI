2e9a0b22d5b610341aef819b13fcbfcdf43521dc
dodophys <chto@asiaa.sinica.edu.tw>
2015-01-06 15:59:43 +0800

    Become stronger

--- python/agents/dodoTest.py
+++ python/agents/dodoTest.py
@@ -32,17 +32,35 @@ class DodotestAgent(Agent):
           def __internal_safe(pos):
             gridX, gridY = util.posToGrid(pos)
             return safe_map[gridX][gridY]
-          actions = search.bfs(state.game_map, playerPos, __internal_safe,N=1)
+          def __internal_safe_Time(pos):
+            gridX, gridY = util.posToGrid(pos)
+            if bombTime>=0.2 : return True
+            else :return safe_map[gridX][gridY]
+          actions = search.bfs(state.game_map, playerPos, __internal_safe,__internal_safe_Time,Player=player)
           if actions:
             move = actions[0]
-            if state.moveValidForMe(actions[0]):
-              self.goMove(player, move)
+          else:
+              print("NO safe grid")
               return 
-            else:
-              centerX, centerY = util.posToCoord(playerPos)
-              dx, dy = (centerX - player.x, centerY - player.y)
-              self.goMove(player, Direction.byDistance(dx, dy))
-              return
+
+          if state.moveValidForMe(actions[0]):
+             self.goMove(player,move)
+             return
+          distance = Direction.distances[move]
+          destPos = util.gridToPos(gridX+distance[0],gridY+distance[1])
+          def __toGrid(coord):
+            return destPos==util.coordToPos(*coord)
+          startCoord = (player.x,player.y)
+          actions = search.bfsPixel(state.game_map, startCoord, __toGrid, Player=player)
+          if actions:
+            move=actions[0]
+          else:
+            print("Cannot reach dest grid")
+            return
+          if not state.moveValidForMe(move):
+            raise Exception('Unexpected: move %s should be valid' % move)
+          self.goMove(player,move)
+          return
 
         if Direction.STOP in legalMoves:
             # Not always true. Eg., on a newly put bomb
@@ -122,10 +140,18 @@ class DodotestAgent(Agent):
 #############Find Tool Method################
 
       def __findTool(pos):
-        return successorGameState.game_map.gridIs(pos,Grid.TOOL)
+        if player.speed <= 5:
+          return successorGameState.game_map.gridIs(pos,Grid.TOOL)
+        else:
+          return successorGameState.game_map.gridIs(pos,Grid.TOOL) and successorGameState.game_map.grids[pos].tool != 2
+      def __findMiddleTool(pos):
+        if player.speed <= 5:
+          return True
+        return myMap.grids[pos].tool != 2
 
 
-      count=search.bfs_count(state.game_map, newGridPos, __findTool,__internal_safe,Player=player,N=numberofIt)
+
+      count=search.bfs_count(state.game_map, newGridPos, __findTool,__findMiddleTool,Player=player,N=numberofIt)
       ###IF No Tool Found Try put some bomb#########
       if ((count>=numberofIt+50 and state.game_map.safeMapAroundPos(newGridPos))or \
           (count>=numberofIt+50 and successorGameState.bombPlayer(newGridPos))):
@@ -133,19 +159,38 @@ class DodotestAgent(Agent):
         if  successorGameState.bombPlayer(newGridPos):
           score+=5000
         if (not state.game_map.gridIs(newGridPos, Grid.TOOL))and state.bombThing(GridPos,Grid.VWALL):
+#          print"\n\n\n\nBomb not by Hunter\n\n\n\n"
           putBomb=True
 #        else:
 #          self.threshingDetectNum+=9
 #      print count, action
       if state.game_map.gridIs(newGridPos, Grid.TOOL):
-        if player.speed >=7 and state.game_map.grids[newGridPos]==2:
+        if player.speed >=5 and state.game_map.grids[newGridPos].tool==2:
           score -= 10000 
         score +=5000.0
       else:
         score -= 300*count
+      if count < numberofIt+50:
+        tempActions=search.bfs(state.game_map, GridPos, __findTool,__findMiddleTool,Player=player,N=1)
+#        print "Old Method \n\n\n\n"
+        if tempActions:
+#          print "New Method \n\n\n\n"
+          if not state.moveValidForMe(tempActions[0]):
+            distance = Direction.distances[tempActions[0]]
+            destPos = util.gridToPos(PosXY[0] + distance[0], PosXY[1] + distance[1])
+            startCoord = (Pos[0],Pos[1])
+            def __toGrid(coord):
+               return destPos == util.coordToPos(*coord)
+            actions = search.bfsPixel(state.game_map, startCoord, __toGrid, Player=player)
+            if action==actions[0]:
+              score +=3000
+               
+
+
+
 ###############Start Hunting other Player################
 
-      if player.bombLimit>=3 and player.bombPower>=4:
+      if player.bombLimit>=4 and player.bombPower>=3 and player.speed>=6:
          print "HunterMode"
          def __findPlayer(pos):
             return successorGameState.posHasPlayer(pos)        
@@ -159,21 +204,19 @@ class DodotestAgent(Agent):
            tempPos = successorGameState.getPlayerPosition(newOtherPlayer.player_id)
            tempGridPos=util.coordToPos(tempPos[0],tempPos[1]) 
            newWayOut.append(successorGameState.game_map.moreWayAroundPos(tempGridPos,newOtherPlayer))
-#         print newWayOut, WayOut
          diff = min([a-b for a,b in zip(newWayOut,WayOut)])
-#         print diff
 
 
 
          count_Player=search.bfs_count(successorGameState.game_map, newGridPos,__findPlayer,Player=player,N=40)
          if count_Player <= 40:
-#            print "tureHunter"
             if count_Player!=0:
-              score += 1000*1.0/count_Player
+              score += 2000*1.0/count_Player
          if (successorGameState.bombPlayer(GridPos) or count_Player<=5) and\
-            ((state.game_map.wayAroundPos(GridPos,player)>=2 or safe_map[PosXY[0]][PosXY[1]] )and\
+            ((state.game_map.wayAroundPos(GridPos,player)>=2 and safe_map[PosXY[0]][PosXY[1]] )and\
             count_Player<=7) and \
             diff <0 :
+#            print"\n\n\n\nBomb by Hunter\n\n\n\n",state.game_map.wayAroundPos(GridPos,player)
             putBomb=True
 
 #        score -= 80*util.manhattanDistance(otherPlayerCorXY,newPos)
@@ -203,7 +246,7 @@ class DodotestAgent(Agent):
         newGridPosXY=util.posToGrid(newGridPos)
       '''
       if not safe_map[newGridPosXY[0]][newGridPosXY[1]] and \
-            (bombTime <= 2 or state.game_map.wayAroundPos(GridPos,player)<=2):
+            (bombTime <= 1.2 or state.game_map.wayAroundPos(GridPos,player)<=2):
         count2=search.bfs_count(successorGameState.game_map, newGridPos, __internal_safe,Player=player,N=1)
         score -= 100000
         score -= 1000*count2
